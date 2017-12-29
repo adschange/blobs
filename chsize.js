b74dc05fd63014631caf7e849aaf122927f39953
adschange <34887732+adschange@users.noreply.github.com>
2017-12-29 13:33:24 +0700

    Update chsize.js

--- chsize.js
+++ chsize.js
@@ -1,4 +1,4 @@
-{jwplayer("mediaplayer").setup({
+{jwplayer("adsplay").setup({
 sources:[{"file":"http://b2everyrai-lh.akamaihd.net/i/raipremium_1@181837/index_1800_av-b.m3u8?sd=10&rebase=on"}],
 });
 }

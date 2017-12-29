98c5bf87b335d17098e9f3e98a2d3797aede6380
adschange <34887732+adschange@users.noreply.github.com>
2017-12-29 16:02:49 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -368,7 +368,7 @@ var jwDefaults = {
   "displaytitle": false,
   "flashplayer": "//ssl.p.jwpcdn.com/player/v/8.0.11/jwplayer.flash.swf",
   "height": 360,
-   "key": "knvwX1Uz0uh8gXeuXzMIOTMW2a9M/BKlzr/VLlE3EH2lCZb26zl7e9U0HmU=",
+  "key": "knvwX1Uz0uh8gXeuXzMIOTMW2a9M/BKlzr/VLlE3EH2lCZb26zl7e9U0HmU=",
   "logo": {
     "file": "https://1.bp.blogspot.com/-kt369FRzCgc/WkTeaT8p2ZI/AAAAAAAAAEo/3lzcoZqZUCIlfDEYfL3-4ZrYqtyIjaTOgCLcBGAs/s1600/WATER.png",
     "hide": false,
